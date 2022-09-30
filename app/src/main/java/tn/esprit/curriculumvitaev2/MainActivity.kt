e700cbe15ff35caac6f1bdbdd645d51d4b6595b0
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2022-09-30 21:54:32 +0100

    Initial commit

---
+++ app/src/main/java/tn/esprit/curriculumvitaev2/MainActivity.kt
@@ -0,0 +1,11 @@
+package tn.esprit.curriculumvitaev2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/tn/esprit/curriculumvitaev2/SplashScreen.kt
@@ -0,0 +1,24 @@
+package tn.esprit.curriculumvitaev2
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(mainLooper).postDelayed({ //This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            // close this activity
+            finish()
+        }, 1500)
+    }
+}
+
+
